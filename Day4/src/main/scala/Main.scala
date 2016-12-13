64dc29c629a9ca9d3c8424ef78656a6719d54791
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-12-13 00:56:33 +0000

    Got part 1 of day 4 working

--- Day4/src/main/scala/Main.scala
+++ Day4/src/main/scala/Main.scala
@@ -1,4 +1,10 @@
+import scala.io.Source
+
 object Main {
   def main(args: Array[String]): Unit = {
+    val lines = Source.fromResource("day4-input.txt").getLines.toList
+    val sectorIds = lines map EncryptedRoomNames.sectorIdOfRoom
+    val sum = sectorIds.flatten.sum
+    println(s"sum: $sum")
   }
 }
