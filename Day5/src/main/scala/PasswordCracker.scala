416f94f2e256fda0ace7e1e1bc7da472814ef681
Jon Taylor <jont@cakesolutions.net>
2016-12-13 23:53:06 +0000

    Got part 2 of day 5 working

--- Day5/src/main/scala/PasswordCracker.scala
+++ Day5/src/main/scala/PasswordCracker.scala
@@ -1,8 +1,14 @@
+import scala.util.{Success, Try}
+
 object PasswordCracker {
 
   def main(args: Array[String]): Unit = {
-    val answer = crackPassword("reyedfim")
-    println(s"answer: $answer")
+
+    val answer1 = crackPassword("reyedfim")
+    println(s"part 1 answer: $answer1")
+
+    val answer2 = crackPassword2("reyedfim")
+    println(s"part 2 answer: $answer2")
   }
 
   def crackPassword(s: String): String = {
@@ -20,6 +26,26 @@ object PasswordCracker {
       }
     }
     loop(0, "")
+  }
+
+  def crackPassword2(s: String): String = {
+    def loop(num: Int, map: Map[Int, Char]): Map[Int, Char] = {
+      if (map.size == 8) map
+      else {
+        val hash = calculateHash(s, num)
+        if (hash.startsWith("00000")) {
+          Try { Integer.parseInt(hash(5).toString) } match {
+            case Success(pos) if pos <= 7 && !map.contains(pos) =>
+              println(s"found char ${hash(6)} at pos $pos")
+              loop(num + 1, map + (pos -> hash(6)))
+            case _ =>
+              loop(num + 1, map)
+          }
+        }
+        else loop(num + 1, map)
+      }
+    }
+    loop(0, Map.empty).toList.sortBy(kvp => kvp._1).map(_._2).mkString
   }
 
   private final val m = java.security.MessageDigest.getInstance("MD5")
--- Day5/src/test/scala/PasswordCrackerTests.scala
+++ Day5/src/test/scala/PasswordCrackerTests.scala
@@ -1,7 +1,12 @@
 import org.scalatest.FlatSpec
 
 class PasswordCrackerTests extends FlatSpec {
-  "abc" should "give a password of 18f47a30" in {
+
+  "abc" should "give a part 1 password of 18f47a30" in {
     assert(PasswordCracker.crackPassword("abc") == "18f47a30")
+  }
+
+  "abc" should "give a part 2 password of 05ace8e3" in {
+    assert(PasswordCracker.crackPassword2("abc") == "05ace8e3")
   }
 }
