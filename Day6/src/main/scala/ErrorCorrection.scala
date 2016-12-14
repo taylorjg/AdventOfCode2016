3353896c5f38cc53fe1927f75eb42845f396caab
Jon Taylor <jont@cakesolutions.net>
2016-12-14 12:46:37 +0000

    Added main to day 6 to see the answer to the main puzzle

--- Day6/src/main/scala/ErrorCorrection.scala
+++ Day6/src/main/scala/ErrorCorrection.scala
@@ -1,4 +1,14 @@
+import scala.io.Source
+
 object ErrorCorrection {
+
+  def main(args: Array[String]): Unit = {
+    val lines = Source.fromResource("day6-input.txt").getLines.toList
+    val cleanedLines = lines map (_.trim) filter (_.nonEmpty)
+    val answer = correct(cleanedLines)
+    println(s"answer: $answer")
+  }
+
   def correct(lines: List[String]): String = {
     def loop(numCols: Int, acc: String): String = {
       if (acc.length == numCols) acc
