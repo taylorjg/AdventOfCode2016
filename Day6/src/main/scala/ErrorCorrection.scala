79e4db6adfb9cbed30b64d8203eabcb1f83f36a5
Jon Taylor <jont@cakesolutions.net>
2016-12-14 12:50:15 +0000

    A little refactoring

--- Day6/src/main/scala/ErrorCorrection.scala
+++ Day6/src/main/scala/ErrorCorrection.scala
@@ -9,16 +9,15 @@ object ErrorCorrection {
     println(s"answer: $answer")
   }
 
+  private final val ReverseOrder = Ordering[Int].reverse
+
   def correct(lines: List[String]): String = {
     def loop(numCols: Int, acc: String): String = {
       if (acc.length == numCols) acc
       else {
         val col = acc.length
-        val v1 = lines map (l => l(col))
-        val v2 = v1.groupBy(identity).toList
-        val v3 = v2.sortBy(kvp => -kvp._2.length)
-        val v4 = v3.head._1
-        val c = v4
+        val sorted = (lines map (_ (col))).groupBy(identity).toList.sortBy(_._2.length)(ReverseOrder)
+        val c = sorted.head._1
         loop(numCols, acc + c)
       }
     }
