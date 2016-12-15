2415a9961687f9fa2162bbede076092f99a8f0ed
Jon Taylor <jont@cakesolutions.net>
2016-12-15 20:03:45 +0000

    Done part 2 of day 8

--- Day8/src/main/scala/Main.scala
+++ Day8/src/main/scala/Main.scala
@@ -2,9 +2,13 @@ import scala.io.Source
 
 object Main {
   def main(args: Array[String]): Unit = {
+
     val instructions = Source.fromResource("day8-input.txt").getLines.toSeq
+
     val screen = Screen.processInstructions(50, 6, instructions)
     val answer = screen.matrix count identity
     println(s"answer: $answer")
+
+    Screen.matrixToString(screen.matrix).grouped(50) foreach println
   }
 }
