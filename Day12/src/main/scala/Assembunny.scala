af1288a619688dadd78da88b3002f7b16dc418d1
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-12-18 03:49:18 +0000

    Done part 1 of day 12

--- Day12/src/main/scala/Assembunny.scala
+++ Day12/src/main/scala/Assembunny.scala
@@ -2,14 +2,17 @@ import scala.util.Try
 
 object Assembunny {
 
-  def execute(code: Vector[String]): Registers =
-    code.indices.foldLeft((new Registers(), Option.empty[Int])) {
-      case (acc @ (registers, jumpIndex), index) => {
-        val newIndex = jumpIndex.getOrElse(index)
-        if (newIndex >= code.length) acc
-        else executeCommand(code, registers, newIndex)
+  def execute(code: Vector[String]): Registers = {
+    @annotation.tailrec
+    def loop(registers: Registers, index: Int): Registers =
+      if (index >= code.length) registers
+      else {
+        val (newRegisters, jumpIndex) = executeCommand(code, registers, index)
+        val newIndex = jumpIndex.getOrElse(index + 1)
+        loop(newRegisters, newIndex)
       }
-    }._1
+    loop(new Registers(), 0)
+  }
 
   private def executeCommand(code: Vector[String], registers: Registers, index: Int): (Registers, Option[Int]) = {
     val line = code(index)
@@ -17,22 +20,19 @@ object Assembunny {
     val b2 = IncRegex.pattern.matcher(line).matches
     val b3 = DecRegex.pattern.matcher(line).matches
     val b4 = JnzRegex.pattern.matcher(line).matches
-    val result = (b1, b2, b3, b4) match {
+    (b1, b2, b3, b4) match {
       case (true, false, false, false) => executeCpyCommand(code, registers, index)
       case (false, true, false, false) => executeIncCommand(code, registers, index)
       case (false, false, true, false) => executeDecCommand(code, registers, index)
       case (false, false, false, true) => executeJnzCommand(code, registers, index)
       case _ => throw new Exception(s"""Unknown instruction, "$line".""")
     }
-    println(s"result: ${result._1.map}; ${result._2}")
-    result
   }
 
   private def executeCpyCommand(code: Vector[String], registers: Registers, index: Int): (Registers, Option[Int]) = {
     val m = CpyRegex.findAllIn(code(index))
     val x = m.group(1)
     val y = m.group(2)
-    println(s"x: $x; y: $y")
     val newValue = Try(x.toInt).toOption match {
       case Some(v) => v
       case None => registers.map(x).value
@@ -43,7 +43,6 @@ object Assembunny {
   private def executeIncCommand(code: Vector[String], registers: Registers, index: Int): (Registers, Option[Int]) = {
     val m = IncRegex.findAllIn(code(index))
     val x = m.group(1)
-    println(s"x: $x")
     val register = registers.map(x)
     (new Registers(registers.map.updated(x, Register(x, register.value + 1))), None)
   }
@@ -51,17 +50,14 @@ object Assembunny {
   private def executeDecCommand(code: Vector[String], registers: Registers, index: Int): (Registers, Option[Int]) = {
     val m = DecRegex.findAllIn(code(index))
     val x = m.group(1)
-    println(s"x: $x")
     val register = registers.map(x)
     (new Registers(registers.map.updated(x, Register(x, register.value - 1))), None)
   }
 
   private def executeJnzCommand(code: Vector[String], registers: Registers, index: Int): (Registers, Option[Int]) = {
     val m = JnzRegex.findAllIn(code(index))
-    println(s"m: $m")
     val x = m.group(1)
     val y = m.group(2).toInt
-    println(s"x: $x; y: $y")
     val jump = Try(x.toInt).toOption match {
       case Some(v) => v != 0
       case None => registers.map(x).value != 0
--- Day12/src/main/scala/Main.scala
+++ Day12/src/main/scala/Main.scala
@@ -1,4 +1,10 @@
+import scala.io.Source
+
 object Main {
   def main(args: Array[String]): Unit = {
+    val code = Source.fromResource("day12-input.txt").getLines.toVector
+    val registers = Assembunny.execute(code)
+    val answer = registers.map("a")
+    println(s"answer: $answer")
   }
 }
