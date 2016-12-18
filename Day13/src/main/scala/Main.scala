7c32ce0c938c95076ea5da46dca81ac1d86b6189
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-12-18 18:49:19 +0000

    Got part 2 of day 13 working

--- Day13/src/main/scala/Main.scala
+++ Day13/src/main/scala/Main.scala
@@ -1,8 +1,19 @@
 object Main {
   def main(args: Array[String]): Unit = {
+
     val seed = 1362
     val maze = new Maze(seed)
-    val answer = maze.bestPathLength(Location(1, 1), Location(31, 39))
+    val startingPoint: Location = Location(1, 1)
+
+    val answer = maze.bestPathLength(startingPoint, Location(31, 39))
     println(s"answer: $answer")
+
+    val answer2 = (for {
+      x <- 0 to 49
+      y <- 0 to 49
+      location = Location(x, y)
+      if maze.locationToCubicle(location).value == CubicleType.OpenSpace
+    } yield location).flatMap(maze.bestPathLength(startingPoint, _)) count (_ <= 50)
+    println(s"answer2: $answer2")
   }
 }
