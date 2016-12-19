36608c32e1268c52aa3bda213f61d53c765c5aeb
Jon Taylor <jont@cakesolutions.net>
2016-12-19 13:29:27 +0000

    More prep for part 2 of day 18 - add caching of hashes that have already been calculated

--- Day14/src/main/scala/OneTimePad.scala
+++ Day14/src/main/scala/OneTimePad.scala
@@ -2,7 +2,9 @@ object OneTimePad {
 
   def findIndex(salt: String, hashType: HashType.Value): Int = {
 
-      def findXxx(hash: String): Option[String] = {
+    type Cache = Map[Int, String]
+
+    def findXxx(hash: String): Option[String] = {
       if (XxxRegex.pattern.matcher(hash).matches()) {
         val m = XxxRegex.findAllIn(hash)
         Some(m.group(1))
@@ -10,41 +12,53 @@ object OneTimePad {
       else None
     }
 
-    def findXxxxx(x: Char, fromIndex: Int, toIndex: Int): Option[Int] = {
+    def findXxxxx(x: Char, fromIndex: Int, toIndex: Int, initialCache: Cache): (Option[Int], Cache) = {
       @annotation.tailrec
-      def loop(index: Int): Option[Int] = {
-        if (index == toIndex) None
+      def loop(index: Int, cache: Cache): (Option[Int], Cache) = {
+        if (index == toIndex) (None, cache)
         else {
-          val hash = calculateHash(salt, index, hashType)
+          val (hash, updatedCache) = cache.get(index) match {
+            case Some(h) =>
+              (h, cache)
+            case None =>
+              val h = calculateHash(salt, index, hashType)
+              (h, cache + (index -> h))
+          }
           val r = s".*($x)\\1{4}.*".r
-          if (r.pattern.matcher(hash).matches()) Some(index)
-          else loop(index + 1)
+          if (r.pattern.matcher(hash).matches()) (Some(index), updatedCache)
+          else loop(index + 1, updatedCache)
         }
       }
-      loop(fromIndex)
+      loop(fromIndex, initialCache)
     }
 
     @annotation.tailrec
-    def loop(index: Int, nth: Int): Int = {
-      if (nth == 65) index -1
+    def loop(index: Int, nth: Int, cache: Cache): Int = {
+      if (nth == 65) index - 1
       else {
-        val hash = calculateHash(salt, index, hashType)
+        val (hash, updatedCache1) = cache.get(index) match {
+          case Some(h) =>
+            (h, cache - index)
+          case None =>
+            val h = calculateHash(salt, index, hashType)
+            (h, cache + (index -> h))
+        }
         findXxx(hash) match {
           case Some(xxx) =>
-            findXxxxx(xxx.head, index + 1, index + 1000) match {
-              case Some(_) => {
+            findXxxxx(xxx.head, index + 1, index + 1000, updatedCache1) match {
+              case (Some(_), updatedCache2) => {
                 println(s"Found key $nth")
-                loop(index + 1, nth + 1)
+                loop(index + 1, nth + 1, updatedCache2)
               }
-              case None => loop(index + 1, nth)
+              case (None, updatedCache2) => loop(index + 1, nth, updatedCache2)
             }
           case None =>
-            loop(index + 1, nth)
+            loop(index + 1, nth, updatedCache1)
         }
       }
     }
 
-    loop(0, 0)
+    loop(0, 0, Map.empty)
   }
 
   private final val XxxRegex = """.*([a-z0-9])\1{2}.*""".r
