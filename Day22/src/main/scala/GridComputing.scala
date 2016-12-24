92d801acd36c8252fc4424853b2931561ecb2ac2
Jon Taylor <jont@cakesolutions.net>
2016-12-24 20:20:37 +0000

    Added unit test re part 2 of day 22

--- Day22/src/main/scala/GridComputing.scala
+++ Day22/src/main/scala/GridComputing.scala
@@ -9,20 +9,20 @@ object GridComputing {
           Node(x.toInt, y.toInt, size.toInt, used.toInt, avail.toInt, use.toInt)
       }
 
-    lines map parseLine
+    lines.drop(2) map parseLine
   }
 
   private final val LineRegex = """/dev/grid/node-x(\d+)-y(\d+)\s+(\d+)T\s+(\d+)T\s+(\d+)T\s+(\d+)%""".r
 
-  def numViableNodes(nodes: Seq[Node]): Int = {
+  def viableNodes(nodes: Seq[Node]): Seq[(Node, Node)] = {
     def findNode(l: (Int, Int)): Node = {
       val (x, y) = l
       nodes.find(n => n.x == x && n.y == y).get
     }
-    def isViable(ls: ((Int, Int), (Int, Int))): Boolean = {
+    def isViable(ls: ((Int, Int), (Int, Int))): Option[(Node, Node)] = {
       val na = findNode(ls._1)
       val nb = findNode(ls._2)
-      na.used > 0 && na.used < nb.avail
+      Some(na, nb) filter (_ => na.used > 0 && na.used < nb.avail)
     }
     val maxX = nodes.maxBy(n => n.x).x
     val maxY = nodes.maxBy(n => n.y).y
@@ -35,6 +35,10 @@ object GridComputing {
       lb <- allCoords
       if la != lb
     } yield (la, lb)
-    allPairs count isViable
+    allPairs flatMap isViable
+  }
+
+  def fewestNumberOfSteps(nodes: Seq[Node]): Int = {
+    -1
   }
 }
--- Day22/src/main/scala/Main.scala
+++ Day22/src/main/scala/Main.scala
@@ -2,9 +2,9 @@ import scala.io.Source
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val lines = Source.fromResource("day22-input.txt").getLines().toSeq.drop(2)
+    val lines = Source.fromResource("day22-input.txt").getLines().toSeq
     val nodes = GridComputing.parseLines(lines)
-    val answer1 = GridComputing.numViableNodes(nodes)
+    val answer1 = GridComputing.viableNodes(nodes).length
     println(s"answer1: $answer1")
   }
 }
