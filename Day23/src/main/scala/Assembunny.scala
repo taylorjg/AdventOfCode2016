3c795e9a4509025fe9d3082b94977c8030d2ca05
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-12-25 17:29:36 +0000

    Some refactoring re part 1 of day 23

--- Day23/src/main/scala/Assembunny.scala
+++ Day23/src/main/scala/Assembunny.scala
@@ -2,29 +2,14 @@ import scala.util.Try
 
 object Assembunny {
 
-  case class State(program: Vector[Instruction], registers: Registers, nextInstruction: Int)
+  case class State(program: Vector[Instruction], registers: Registers, instructionPointer: Int)
 
-  sealed trait Instruction {
-    self: NumArguments =>
-  }
-
-  sealed trait NumArguments {
-    val numArguments: Int
-  }
-
-  sealed trait OneArgument extends NumArguments {
-    val numArguments = 1
-  }
-
-  sealed trait TwoArguments extends NumArguments {
-    val numArguments = 2
-  }
-
-  case class Cpy(x: String, y: String) extends Instruction with TwoArguments
-  case class Inc(x: String) extends Instruction with OneArgument
-  case class Dec(x: String) extends Instruction with OneArgument
-  case class Jnz(x: String, y: String) extends Instruction with TwoArguments
-  case class Tgl(x: String) extends Instruction with OneArgument
+  sealed trait Instruction
+  case class Cpy(x: String, y: String) extends Instruction
+  case class Inc(x: String) extends Instruction
+  case class Dec(x: String) extends Instruction
+  case class Jnz(x: String, y: String) extends Instruction
+  case class Tgl(x: String) extends Instruction
 
   def parseProgram(program: Seq[String]): Vector[Instruction] = {
     def parseLine(line: String): Instruction = {
@@ -43,15 +28,17 @@ object Assembunny {
   def execute(program: Vector[Instruction], rs: Register*): Registers = {
     @annotation.tailrec
     def loop(state: State): State =
-      if (state.nextInstruction >= program.length) state else loop(executeCommand(state))
+      if (state.instructionPointer >= program.length) state else loop(executeCommand(state))
     val initialRegisters = new Registers(rs)
     val initialState = State(program, initialRegisters, 0)
     val finalState = loop(initialState)
     finalState.registers
   }
 
+  private final val NextInstructionOffset = 1
+
   private def executeCommand(state: State): State = {
-    val instruction = state.program(state.nextInstruction)
+    val instruction = state.program(state.instructionPointer)
     val (newState, maybeJumpOffset) = instruction match {
       case Cpy(x, y) => executeCpyInstruction(state, x, y)
       case Inc(x) => executeIncInstruction(state, x)
@@ -59,14 +46,11 @@ object Assembunny {
       case Jnz(x, y) => executeJnzInstruction(state, x, y)
       case Tgl(x) => executeTglInstruction(state, x)
     }
-    newState.copy(nextInstruction = state.nextInstruction + maybeJumpOffset.getOrElse(1))
+    newState.copy(instructionPointer = state.instructionPointer + maybeJumpOffset.getOrElse(NextInstructionOffset))
   }
 
   private def executeCpyInstruction(state: State, x: String, y: String): (State, Option[Int]) = {
-    val newValue = Try(x.toInt).toOption match {
-      case Some(v) => v
-      case None => state.registers.getValue(x)
-    }
+    val newValue = valueOfLiteralOrRegister(state, x)
     Try(y.toInt).toOption match {
       case Some(_) =>
         (state, None)
@@ -101,29 +85,20 @@ object Assembunny {
   }
 
   private def executeJnzInstruction(state: State, x: String, y: String): (State, Option[Int]) = {
-    val jump = Try(x.toInt).toOption match {
-      case Some(v) => v != 0
-      case None => state.registers.getValue(x) != 0
-    }
-    val offset = Try(y.toInt).toOption match {
-      case Some(v) => v
-      case None => state.registers.getValue(y)
-    }
-    val maybeJumpOffset = if (jump) Some(offset) else None
+    val nz = valueOfLiteralOrRegister(state, x) != 0
+    val jumpOffset = valueOfLiteralOrRegister(state, y)
+    val maybeJumpOffset = Some(jumpOffset) filter (_ => nz)
     (state, maybeJumpOffset)
   }
 
   private def executeTglInstruction(state: State, x: String): (State, Option[Int]) = {
-    val offset = Try(x.toInt).toOption match {
-      case Some(v) => v
-      case None => state.registers.getValue(x)
-    }
-    val newProgram = toggleInstruction(state, offset)
+    val targetInstructionOffset = valueOfLiteralOrRegister(state, x)
+    val newProgram = toggleInstruction(state, targetInstructionOffset)
     (state.copy(program = newProgram), None)
   }
 
   private def toggleInstruction(state: State, offset: Int): Vector[Instruction] = {
-    val targetInstructionIndex = state.nextInstruction + offset
+    val targetInstructionIndex = state.instructionPointer + offset
     if (!state.program.isDefinedAt(targetInstructionIndex)) state.program
     else {
       val newInstruction = state.program(targetInstructionIndex) match {
@@ -139,6 +114,12 @@ object Assembunny {
       newInstructions
     }
   }
+
+  private def valueOfLiteralOrRegister(state: State, s: String): Int =
+    Try(s.toInt).toOption match {
+      case Some(v) => v
+      case None => state.registers.getValue(s)
+    }
 
   private final val CpyRegex = """cpy (-?\d+|[a-d]) ([a-d])""".r
   private final val IncRegex = """inc ([a-d])""".r
