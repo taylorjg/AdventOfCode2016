3d3d04614b136a0800b6b2c470e4ce7e5ecea8f3
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-12-25 10:58:17 +0000

    Refactored old assembunny code

--- Day23/src/main/scala/Assembunny.scala
+++ Day23/src/main/scala/Assembunny.scala
@@ -2,68 +2,64 @@ import scala.util.Try
 
 object Assembunny {
 
+  case class State(code: Vector[String], registers: Registers, nextInstruction: Int)
+
   def execute(code: Vector[String], rs: Register*): Registers = {
     @annotation.tailrec
-    def loop(registers: Registers, index: Int): Registers =
-      if (index >= code.length) registers
-      else {
-        val (newRegisters, jumpIndex) = executeCommand(code, registers, index)
-        val newIndex = jumpIndex.getOrElse(index + 1)
-        loop(newRegisters, newIndex)
-      }
+    def loop(state: State): State = if (state.nextInstruction >= code.length) state else loop(executeCommand(state))
     val initialRegisters = new Registers(new Registers().map ++ (rs map (r => (r.name, r))))
-    loop(initialRegisters, 0)
+    val initialState = State(code, initialRegisters, 0)
+    val finalState = loop(initialState)
+    finalState.registers
   }
 
-  private def executeCommand(code: Vector[String], registers: Registers, index: Int): (Registers, Option[Int]) = {
-    val line = code(index)
-    line match {
-      case CpyRegex(x, y) => executeCpyCommand(code, registers, index, x, y)
-      case IncRegex(x) => executeIncCommand(code, registers, index, x)
-      case DecRegex(x) => executeDecCommand(code, registers, index, x)
-      case JnzRegex(x, y) => executeJnzCommand(code, registers, index, x, y.toInt)
-      case TglRegex(x) => executeTglCommand(code, registers, index, x)
+  private def executeCommand(state: State): State = {
+    val line = state.code(state.nextInstruction)
+    val (newState, maybeJumpIndex) = line match {
+      case CpyRegex(x, y) => executeCpyCommand(state, x, y)
+      case IncRegex(x) => executeIncCommand(state, x)
+      case DecRegex(x) => executeDecCommand(state, x)
+      case JnzRegex(x, y) => executeJnzCommand(state, x, y.toInt)
+      case TglRegex(x) => executeTglCommand(state, x)
       case _ => throw new Exception(s"""Unknown instruction, "$line".""")
     }
+    newState.copy(nextInstruction = maybeJumpIndex.getOrElse(state.nextInstruction + 1))
   }
 
-  private def executeCpyCommand(code: Vector[String], registers: Registers, index: Int, x: String, y: String): (Registers, Option[Int]) = {
+  private def executeCpyCommand(state: State, x: String, y: String): (State, Option[Int]) = {
     val newValue = Try(x.toInt).toOption match {
       case Some(v) => v
-      case None => registers.map(x).value
+      case None => state.registers.map(x).value
     }
-    (new Registers(registers.map.updated(y, Register(y, newValue))), None)
+    val newRegisters = new Registers(state.registers.map.updated(y, Register(y, newValue)))
+    (state.copy(registers = newRegisters), None)
   }
 
-  private def executeIncCommand(code: Vector[String], registers: Registers, index: Int, x: String): (Registers, Option[Int]) = {
-    val m = IncRegex.findAllIn(code(index))
-    val x = m.group(1)
-    val register = registers.map(x)
-    (new Registers(registers.map.updated(x, Register(x, register.value + 1))), None)
+  private def executeIncCommand(state: State, x: String): (State, Option[Int]) = {
+    val register = state.registers.map(x)
+    val newRegisters: Registers = new Registers(state.registers.map.updated(x, Register(x, register.value + 1)))
+    (state.copy(registers = newRegisters), None)
   }
 
-  private def executeDecCommand(code: Vector[String], registers: Registers, index: Int, x: String): (Registers, Option[Int]) = {
-    val m = DecRegex.findAllIn(code(index))
-    val x = m.group(1)
-    val register = registers.map(x)
-    (new Registers(registers.map.updated(x, Register(x, register.value - 1))), None)
+  private def executeDecCommand(state: State, x: String): (State, Option[Int]) = {
+    val register = state.registers.map(x)
+    val newRegisters: Registers = new Registers(state.registers.map.updated(x, Register(x, register.value - 1)))
+    (state.copy(registers = newRegisters), None)
   }
 
-  private def executeJnzCommand(code: Vector[String], registers: Registers, index: Int, x: String, y: Int): (Registers, Option[Int]) = {
+  private def executeJnzCommand(state: State, x: String, y: Int): (State, Option[Int]) = {
     val jump = Try(x.toInt).toOption match {
       case Some(v) => v != 0
-      case None => registers.map(x).value != 0
+      case None => state.registers.map(x).value != 0
     }
-    val newIndex = if (jump) Some(index + y) else None
-    (registers, newIndex)
+    val newIndex = if (jump) Some(state.nextInstruction + y) else None
+    (state, newIndex)
   }
 
-  private def executeTglCommand(code: Vector[String], registers: Registers, index: Int, x: String): (Registers, Option[Int]) = {
-    val m = TglRegex.findAllIn(code(index))
-    val x = m.group(1)
+  private def executeTglCommand(state: State, x: String): (State, Option[Int]) = {
     val newIndex = Try(x.toInt).toOption match {
       case Some(v) => v
-      case None => registers.map(x).value
+      case None => state.registers.map(x).value
     }
     ???
   }
@@ -74,3 +70,19 @@ object Assembunny {
   private final val JnzRegex = """jnz (\d+|[a-d]) (-?\d+)""".r
   private final val TglRegex = """tgl (\d+|[a-d])""".r
 }
+
+// one argument instructions:
+//
+//   inc  => dec
+//
+//   dec  => inc
+//   tgl  => inc
+//   *    => inc
+//
+// two argument instructions:
+//
+//   jnz  => cpy
+//
+//   cpy  => jnz
+//   *    => jnz
+//
