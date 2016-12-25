0d5e55ac4fbf6891ef51449fb8a83a4fa869b1d9
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-12-25 20:01:34 +0000

    Prep for part 2 of day 23

--- Day23/src/main/scala/Main.scala
+++ Day23/src/main/scala/Main.scala
@@ -2,9 +2,14 @@ import scala.io.Source
 
 object Main {
   def main(args: Array[String]): Unit = {
+
     val lines = Source.fromResource("day23-input.txt").getLines.toSeq
     val program = Assembunny.parseProgram(lines)
+
     val answer1 = Assembunny.execute(program, Register("a", 7)).a
     println(s"answer1: $answer1")
+
+    val answer2 = Assembunny.execute(program, Register("a", 12)).a
+    println(s"answer2: $answer2")
   }
 }
